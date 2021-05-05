548e452b7014d09fc6c5a20e9d24e0ea8539979e
Emmanuel Evuazeze <evuazeze.emmanuel@gmail.com>
2021-05-05 18:20:02 +0100

    enable cors retry retry retry retry retry retry retry retry

--- src/utils/StarWarsAPI.js
+++ src/utils/StarWarsAPI.js
@@ -1,9 +1,16 @@
+const api = 'http://swapi.dev/api'
+
+let token = localStorage.token
+
+if (!token)
+    token = localStorage.token = Math.random().toString(36).substr(-8)
+
 const headers = {
     'Accept': 'application/json'
 }
 
 export const getAll = () =>
-    fetch(`/people`)
+    fetch(`${api}/people`, { headers })
         .then(res => res.json())
         .then(data => data.results)
 
